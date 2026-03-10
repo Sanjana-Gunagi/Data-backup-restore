9ae803f74eacd4ee94afcd1f8e6b55b52867e3f0
Sanjana-Gunagi <sanjanagunagi59@gmail.com>
2026-03-10 18:36:14 +0530

    Update authController.js

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,1 +1,19 @@
-f
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+const { createUser } = require("../models/userModel");
+
+exports.register = async (req, res) => {
+
+  const { username, email, password } = req.body;
+
+  const hashedPassword = await bcrypt.hash(password, 10);
+
+  createUser(username, email, hashedPassword, (err) => {
+
+    if (err) return res.status(500).json(err);
+
+    res.json({ message: "User registered successfully" });
+
+  });
+
+};
