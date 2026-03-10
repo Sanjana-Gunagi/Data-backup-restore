b99b59d1a68ae5d5cee8af2208d2e4968d2f09e3
Sanjana-Gunagi <sanjanagunagi59@gmail.com>
2026-03-10 18:45:26 +0530

    Update backupController.js

--- controllers/backupController.js
+++ controllers/backupController.js
@@ -1,1 +1,99 @@
-g
+const db = require("../config/db");
+const crypto = require("crypto");
+const fs = require("fs");
+const path = require("path");
+
+exports.uploadFile = (req, res) => {
+
+  const userId = req.user.id;
+  const file = req.file;
+
+  const fileBuffer = fs.readFileSync(file.path);
+
+  // Generate SHA256 hash
+  const hash = crypto
+    .createHash("sha256")
+    .update(fileBuffer)
+    .digest("hex");
+
+  db.query(
+    "SELECT * FROM backups WHERE user_id = ? AND filename = ? ORDER BY version DESC LIMIT 1",
+    [userId, file.originalname],
+    (err, result) => {
+
+      if (err) return res.status(500).json(err);
+
+      // Check duplicate
+      if (result.length > 0 && result[0].file_hash === hash) {
+        fs.unlinkSync(file.path);
+        return res.json({ message: "No changes detected. Backup skipped." });
+      }
+
+      const newVersion = result.length > 0 ? result[0].version + 1 : 1;
+
+      db.query(
+        "INSERT INTO backups (user_id, filename, filepath, file_hash, version) VALUES (?, ?, ?, ?, ?)",
+        [userId, file.originalname, file.path, hash, newVersion],
+        (err2) => {
+
+          if (err2) return res.status(500).json(err2);
+
+          res.json({
+            message: "File backed up successfully",
+            version: newVersion
+          });
+
+        }
+      );
+
+    }
+  );
+
+};
+
+
+exports.getBackups = (req, res) => {
+
+  db.query(
+    "SELECT * FROM backups WHERE user_id = ?",
+    [req.user.id],
+    (err, result) => {
+
+      if (err) return res.status(500).json(err);
+
+      res.json(result);
+
+    }
+  );
+
+};
+
+
+exports.restoreFile = (req, res) => {
+
+  const backupId = req.params.id;
+
+  db.query(
+    "SELECT * FROM backups WHERE id = ? AND user_id = ?",
+    [backupId, req.user.id],
+    (err, result) => {
+
+      if (err) return res.status(500).json(err);
+
+      if (result.length === 0)
+        return res.status(404).json({ message: "Backup not found" });
+
+      const relativePath = result[0].filepath;
+
+      const absolutePath = path.join(__dirname, "..", relativePath);
+
+      if (!fs.existsSync(absolutePath)) {
+        return res.status(404).json({ message: "File not found on server" });
+      }
+
+      res.download(absolutePath, result[0].filename);
+
+    }
+  );
+
+};
