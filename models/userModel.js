ce90cd30cafc2917e771bd58db53e5e929ca4dd3
Sanjana-Gunagi <sanjanagunagi59@gmail.com>
2026-03-10 18:35:31 +0530

    Update userModel.js

--- models/userModel.js
+++ models/userModel.js
@@ -1,1 +1,8 @@
-i
+const db = require("../config/db");
+
+const createUser = (username, email, password, callback) => {
+  const sql = "INSERT INTO users (username,email,password) VALUES (?,?,?)";
+  db.query(sql, [username, email, password], callback);
+};
+
+module.exports = { createUser };
