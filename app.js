077b517e36444ddf63d6905233043bf462b6458e
Sanjana-Gunagi <sanjanagunagi59@gmail.com>
2026-03-10 19:17:34 +0530

    Create app.js

---
+++ app.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const cors = require("cors");
+
+const authRoutes = require("./routes/authRoutes");
+const backupRoutes = require("./routes/backupRoutes");
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.use("/api/auth", authRoutes);
+app.use("/api/backup", backupRoutes);
+
+app.listen(5000, () => {
+  console.log("Server running on port 5000");
+});
