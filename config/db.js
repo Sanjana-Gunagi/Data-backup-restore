f1e8e3e0ccd29cf7a307651e5aeef4b8b16ba92e
Sanjana-Gunagi <sanjanagunagi59@gmail.com>
2026-03-10 18:35:12 +0530

    Update db.js

--- config/db.js
+++ config/db.js
@@ -1,1 +1,18 @@
-c
+const mysql = require("mysql2");
+
+const db = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "",
+  database: "backup_system"
+});
+
+db.connect(err => {
+  if (err) {
+    console.log("Database connection failed");
+  } else {
+    console.log("Database connected");
+  }
+});
+
+module.exports = db;
