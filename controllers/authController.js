433e9202ee7ee17c6e94426a395e24e7aadabeb9
Sanjana-Gunagi <sanjanagunagi59@gmail.com>
2026-03-10 19:07:09 +0530

    Update authController.js

--- controllers/authController.js
+++ controllers/authController.js
@@ -17,3 +17,31 @@ exports.register = async (req, res) => {
   });
 
 };
+
+exports.login = (req, res) => {
+
+  const db = require("../config/db");
+  const bcrypt = require("bcrypt");
+  const jwt = require("jsonwebtoken");
+
+  const { email, password } = req.body;
+
+  db.query(
+    "SELECT * FROM users WHERE email = ?",
+    [email],
+    async (err, result) => {
+
+      if (err) return res.status(500).json(err);
+
+      if (result.length === 0)
+        return res.status(404).json({ message: "User not found" });
+
+      const user = result[0];
+
+      const match = await bcrypt.compare(password, user.password);
+
+      if (!match)
+        return res.status(401).json({ message: "Invalid password" });
+
+      const token = jwt.sign(
+        {
