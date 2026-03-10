47f82f64f9e0b7c4396b7488f4e0b11f782252a1
Sanjana-Gunagi <sanjanagunagi59@gmail.com>
2026-03-10 18:36:37 +0530

    Update authRouts.js

--- routes/authRouts.js
+++ routes/authRouts.js
@@ -1,1 +1,8 @@
-e
+const express = require("express");
+const router = express.Router();
+
+const { register } = require("../controllers/authController");
+
+router.post("/register", register);
+
+module.exports = router;
