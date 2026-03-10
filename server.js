4a0558b3381e2de9c1af50c8abf7f30ed2adf269
Sanjana-Gunagi <sanjanagunagi59@gmail.com>
2026-03-10 18:34:51 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,1 +1,14 @@
-a
+const express = require("express");
+const app = express();
+
+const authRoutes = require("./routes/authRoutes");
+const backupRoutes = require("./routes/backupRoutes");
+
+app.use(express.json());
+
+app.use("/api/auth", authRoutes);
+app.use("/api/backup", backupRoutes);
+
+app.listen(5000, () => {
+  console.log("Server running on port 5000");
+});
